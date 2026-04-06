22c0c1665335a6c3a41a2f2ba8d4162e240d5e15
Shazad <shezad.demonoid@gmail.com>
2026-04-06 12:53:21 +0400

    feat: setup routing and main layout shell
    - configure react-router routes
    - add MainLayout with sidebar + outlet
    - implement navigation with NavLink
    - create base pages (Home, Products, Users, Employees)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
-import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Users from "./pages/Users";
+import Employees from "./pages/Employees";
 
 function App() {
   return (
-    <>
-      <Sidebar />
-    </>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/employees" element={<Employees />} />
+      </Route>
+    </Routes>
   );
 }
 
